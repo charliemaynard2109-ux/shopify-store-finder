28e8ef9d3d5c6027df7bc27b56683e1ba44f0dc6
charliemaynard2109-ux <charliemaynard2109@googlemail.com>
2026-07-14 19:06:58 +0100

    Create postcode_batches.py

---
+++ scanner/postcode_batches.py
@@ -0,0 +1,29 @@
+POSTCODE_BATCHES = {
+
+    "batch1": [
+        "CM1",
+        "CM2",
+        "CM3",
+        "CM4",
+        "CM5"
+    ],
+
+
+    "batch2": [
+        "CM6",
+        "CM7",
+        "CM8",
+        "CM9",
+        "CM10"
+    ],
+
+
+    "batch3": [
+        "SS1",
+        "SS2",
+        "SS3",
+        "SS4",
+        "SS5"
+    ]
+
+}

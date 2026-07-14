7486089403a5d3ca4358aa75338aebb740d066c1
charliemaynard2109-ux <charliemaynard2109@googlemail.com>
2026-07-14 17:18:34 +0100

    Update scanner.py

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,26 +1,48 @@
 import requests
 import json
-
+import time
 from detector import detect_platform
 
 
+# UK postcode prefixes
+POSTCODES = [
+    "AB","AL","B","BA","BB","BD","BH","BL","BN","BR","BS",
+    "BT","CA","CB","CF","CH","CM","CO","CR","CT","CV",
+    "CW","DA","DD","DE","DG","DH","DL","DN","DT","DY",
+    "E","EC","EH","EN","EX","FK","FY","G","GL","GU",
+    "HA","HD","HG","HP","HR","HS","HX","IG","IP","IV",
+    "KA","KT","KW","KY","L","LA","LD","LE","LL","LN",
+    "LS","LU","M","ME","MK","ML","N","NE","NG","NN",
+    "NP","NR","NW","OL","OX","PA","PE","PH","PL",
+    "PO","PR","RG","RH","RM","S","SA","SE","SG",
+    "SK","SL","SM","SN","SO","SP","SR","SS","ST",
+    "SW","SY","TA","TD","TF","TN","TQ","TR","TS",
+    "TW","UB","WA","WC","WD","WF","WN","WR","WS",
+    "WV","YO","ZE"
+]
 
-POSTCODE="CM1"
+
+def find_businesses(prefix):
+
+    print("Searching:", prefix)
 
 
-def find_businesses(postcode):
+    query = f"""
 
+[out:json][timeout:60];
 
-    query=f"""
-
-[out:json];
-
-area["name"="{postcode}"]->.searchArea;
+area["ISO3166-1"="GB"]->.uk;
 
 (
-node["shop"](area.searchArea);
-node["name"](area.searchArea);
-way["shop"](area.searchArea);
+node["shop"](area.uk);
+way["shop"](area.uk);
+
+node["craft"](area.uk);
+way["craft"](area.uk);
+
+node["office"](area.uk);
+way["office"](area.uk);
+
 );
 
 out center;
@@ -28,60 +50,87 @@ out center;
 """
 
 
+    try:
 
-    url="https://overpass-api.de/api/interpreter"
+        response = requests.post(
+            "https://overpass-api.de/api/interpreter",
+            data=query,
+            timeout=120
+        )
 
 
-    response=requests.post(
-        url,
-        data=query
-    )
+        data=response.json()
 
 
-    data=response.json()
+    except Exception as e:
+
+        print("OSM error:", e)
+        return []
+
 
 
     businesses=[]
 
 
-    for item in data["elements"]:
+    for item in data.get("elements", []):
 
 
-        tags=item.get("tags",{})
+        tags=item.get("tags", {})
 
 
         name=tags.get("name")
 
 
-        website=tags.get("website")
+        website=(
+            tags.get("website")
+            or
+            tags.get("contact:website")
+        )
+
+
+        if not name:
+
+            continue
+
+
+        if not website:
+
+            continue
 
 
 
-        if name and website:
+        print(
+            "Checking:",
+            name,
+            website
+        )
 
 
-            platform=detect_platform(
-                website
-            )
+        platform=detect_platform(website)
 
 
-            businesses.append({
 
-                "business":name,
+        businesses.append({
 
-                "address":
-                tags.get("addr:street",""),
+            "business": name,
 
-                "postcode":
-                postcode,
+            "address":
+            tags.get("addr:street",""),
 
-                "website":
-                website,
+            "postcode":
+            prefix,
 
-                "platform":
-                platform
+            "website":
+            website,
 
-            })
+            "platform":
+            platform
+
+        })
+
+
+        time.sleep(0.5)
+
 
 
     return businesses
@@ -89,26 +138,54 @@ out center;
 
 
 
-results=find_businesses(POSTCODE)
+
+def main():
+
+
+    all_businesses=[]
+
+
+    for postcode in POSTCODES:
+
+
+        results=find_businesses(postcode)
+
+
+        all_businesses.extend(results)
+
+
+        print(
+            postcode,
+            "found",
+            len(results)
+        )
 
 
 
-with open(
-"../database.json",
-"w",
-encoding="utf8"
-) as f:
+    with open(
+        "../database.json",
+        "w",
+        encoding="utf-8"
+    ) as file:
 
 
-    json.dump(
-        results,
-        f,
-        indent=2
+        json.dump(
+            all_businesses,
+            file,
+            indent=2,
+            ensure_ascii=False
+        )
+
+
+
+    print(
+        "COMPLETE:",
+        len(all_businesses),
+        "businesses saved"
     )
 
 
-print(
-"Saved",
-len(results),
-"businesses"
-)
+
+if __name__=="__main__":
+
+    main()

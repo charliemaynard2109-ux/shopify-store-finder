6d457bae69a3af051b5aec3af44b3ab75c75d118
charliemaynard2109-ux <charliemaynard2109@googlemail.com>
2026-07-15 09:34:07 +0100

    Update postcode_batches.py

--- scanner/postcode_batches.py
+++ scanner/postcode_batches.py
@@ -1,29 +1,102 @@
+# UK postcode district batches
+# Add/remove districts here.
+# Workflow selects which batch to run.
+
 POSTCODE_BATCHES = {
 
+
     "batch1": [
+
         "CM1",
         "CM2",
         "CM3",
         "CM4",
         "CM5"
+
     ],
 
 
+
     "batch2": [
+
         "CM6",
         "CM7",
         "CM8",
         "CM9",
         "CM10"
+
     ],
 
 
+
     "batch3": [
+
         "SS1",
         "SS2",
         "SS3",
         "SS4",
         "SS5"
+
+    ],
+
+
+
+    "batch4": [
+
+        "SS6",
+        "SS7",
+        "SS8",
+        "SS9",
+        "SS11"
+
+    ],
+
+
+
+    "batch5": [
+
+        "DA1",
+        "DA2",
+        "DA3",
+        "DA4",
+        "DA5"
+
+    ],
+
+
+
+    "batch6": [
+
+        "RM1",
+        "RM2",
+        "RM3",
+        "RM4",
+        "RM5"
+
+    ],
+
+
+
+    "batch7": [
+
+        "RM6",
+        "RM7",
+        "RM8",
+        "RM9",
+        "RM10"
+
+    ],
+
+
+
+    "batch8": [
+
+        "CO1",
+        "CO2",
+        "CO3",
+        "CO4",
+        "CO5"
+
     ]
 
 }
